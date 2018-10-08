8251d967dca0cfc778ebbe36b057700b4e606b58
ech0r <jrcoop34@gmail.com>
2018-10-07 21:26:16 -0700

    still working on backprop

--- NeuralNet.py
+++ NeuralNet.py
@@ -9,6 +9,7 @@ class NeuralNet:
 
     def __init__(self, input_size, output_size, hidden_size, num_hidden):
         self.z = 0
+        self.z_array = []
         self.activated_z = 0
         self.num_hidden = num_hidden - 1
         if self.num_hidden <= 1:
@@ -34,34 +35,43 @@ class NeuralNet:
 
     def feed_forward(self, input):
         i = 0
-        for x in (self.num_hidden + 2):
+        for x in range(self.num_hidden + 2):
             if x == 0:  # input layer -> first hidden layer
                 self.z = np.dot(input, self.input_weight)
                 self.activated_z = self.sigmoid(self.z)
+                self.z_array.append(self.activated_z)
             elif x == (self.num_hidden + 1):  # final hidden layer -> output
                 self.z = np.dot(self.activated_z, self.output_weight)
                 self.activated_z = self.sigmoid(self.z)
+                self.z_array.append(self.activated_z)
                 return self.activated_z
             else:  # hidden layer -> hidden layer
                 self.z = np.dot(self.activated_z, self.hidden_weights[i])
                 self.activated_z = self.sigmoid(self.z)
+                self.z_array.append(self.activated_z)
                 i += 1
 
-
-
-        return output
-
-    # TODO: write backpropagation function
-    def back_prop(self, traininginputarray, testoutputarray, outputarray):
+    def back_prop(self, traininginputarray, expectedoutputarray, outputarray):
         # subtract NN output from known value
-        error = testoutputarray - outputarray
+        ax = 0
+        mse = ((expectedoutputarray - outputarray))**2).mean(axis=ax)
         # get activated error
         error_d = error*self.sigmoid_d(outputarray)
         # TODO: need to figure out how to correctly adjust n hidden layer's weights
-        for x in self.hidden_weights:
-            x_error = error_d.dot(x.T)
-            x_delta = x_error*self.sigmoid_d()
-
+        i = 0
+        for x in range(self.num_hidden + 2):
+            if x == 0:  # input -> hidden
+                z_error = error_d.dot(self.input_weight.T)
+                z_d = z_error*self.sigmoid_d(self.z_array[x])
+                self.z_array[x] += traininginputarray.T.dot(z_d)
+            elif x == self.num_hidden + 1:  # hidden -> output
+                z_error = error_d.dot(self.output_weight.T)
+                z_d = z_error*self.sigmoid_d(self.z_array[x])
+                self.z_array[x] += traininginputarray.T.dot(z_d)
+            else:  # hidden -> hidden
+                z_error = error_d.dot(self.z_array[x].T)
+                z_d = z_error*self.sigmoid_d(self.z_array[x])
+                self.z_array[x] +=
 
     # TODO: write training function
 
