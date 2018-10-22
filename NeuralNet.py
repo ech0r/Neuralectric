bc27a34caf5bc9e81c4495eef500be68bed97b03
ech0r <jrcoop34@gmail.com>
2018-10-21 22:52:19 -0700

    new commit

--- NeuralNet.py
+++ NeuralNet.py
@@ -7,73 +7,48 @@ import numpy as np
 
 class NeuralNet:
 
-    def __init__(self, input_size, output_size, hidden_size, num_hidden):
-        self.z = 0
-        self.z_array = []
-        self.activated_z = 0
-        self.num_hidden = num_hidden - 1
-        if self.num_hidden <= 1:
-            self.num_hidden = 0
-        self.input_size = input_size
-        self.output_size = output_size
-        self.hidden_size = hidden_size
-        # list to store hidden layer weights
-        self.hidden_weights = []
-        # weights from input layer to first hidden layer matrix input_size x hidden_size
-        self.input_weight = np.random.randn(self.input_size, self.hidden_size)
-        # for each hidden layer - generate weights
-        for x in self.num_hidden:
-            self.hidden_weights.append(np.random.randn(self.hidden_size, self.hidden_size))
-        self.output_weight = np.random.randn(self.hidden_size, self.output_size)
+    def __init__(self, input_size, output_size, hidden_size, num_hidden, bias):
+        # array of outputs through network
+        self.y = []
+        self.weightedsum = []
+        self.delta = []
+        self.bias = np.zeros()
+        # build array of weight matrices
+        self.weights = []
+        for i in range(num_hidden):
+            if i == 0:
+                self.weights.append(np.random.rand(input_size, hidden_size))
+            elif i == (num_hidden-1):
+                self.weights.append(np.random.rand(hidden_size, output_size))
+            else:
+                self.weights.append(np.random.rand(hidden_size,hidden_size))
 
-    # TODO: implement more advanced activation functions like ReLU
+    # TODO: implement more advanced activation functions like leaky ReLU
     def sigmoid(self, x):
         return math.exp(-np.logaddexp(0, -x))
 
     def sigmoid_d(self, x):
         return x*(1-x)
 
-    def feed_forward(self, input):
-        i = 0
-        for x in range(self.num_hidden + 2):
-            if x == 0:  # input layer -> first hidden layer
-                self.z = np.dot(input, self.input_weight)
-                self.activated_z = self.sigmoid(self.z)
-                self.z_array.append(self.activated_z)
-            elif x == (self.num_hidden + 1):  # final hidden layer -> output
-                self.z = np.dot(self.activated_z, self.output_weight)
-                self.activated_z = self.sigmoid(self.z)
-                self.z_array.append(self.activated_z)
-                return self.activated_z
-            else:  # hidden layer -> hidden layer
-                self.z = np.dot(self.activated_z, self.hidden_weights[i])
-                self.activated_z = self.sigmoid(self.z)
-                self.z_array.append(self.activated_z)
-                i += 1
+    def feed_forward(self, inputarray):
+        for i in range(len(self.weights)):
+            if i == 0:
+                self.weightedsum.append(np.dot(inputarray,self.weights[i]))
+                self.y.append(self.sigmoid(self.weightedsum[i]))
+            else:
+                self.weightedsum.append(np.dot(self.y[i-1], self.weights[i]))
+                self.y.append(self.sigmoid(self.weightedsum[i]))
 
-    def back_prop(self, traininginputarray, expectedoutputarray, outputarray):
-        # subtract NN output from known value
-        ax = 0
-        mse = ((expectedoutputarray - outputarray))**2).mean(axis=ax)
-        # get activated error
-        error_d = error*self.sigmoid_d(outputarray)
-        # TODO: need to figure out how to correctly adjust n hidden layer's weights
-        i = 0
-        for x in range(self.num_hidden + 2):
-            if x == 0:  # input -> hidden
-                z_error = error_d.dot(self.input_weight.T)
-                z_d = z_error*self.sigmoid_d(self.z_array[x])
-                self.z_array[x] += traininginputarray.T.dot(z_d)
-            elif x == self.num_hidden + 1:  # hidden -> output
-                z_error = error_d.dot(self.output_weight.T)
-                z_d = z_error*self.sigmoid_d(self.z_array[x])
-                self.z_array[x] += traininginputarray.T.dot(z_d)
-            else:  # hidden -> hidden
-                z_error = error_d.dot(self.z_array[x].T)
-                z_d = z_error*self.sigmoid_d(self.z_array[x])
-                self.z_array[x] +=
+    def back_prop(self, trainingdata, expectedoutput):
+        self.delta.append((self.y[-1] - expectedoutput)*self.sigmoid_d(self.weightedsum[-1])*self.y[-2])
+
+
+
+
+
 
     # TODO: write training function
+    def train(self, input, expectedoutput, output):
 
     # TODO: write function to save weights after training
 
