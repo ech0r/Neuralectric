731189755e9509abfeac59413e16f92bef7936a4
ech0r <jrcoop34@gmail.com>
2018-10-24 02:18:25 -0700

    latest commit

--- NeuralNet.py
+++ NeuralNet.py
@@ -7,23 +7,24 @@ import numpy as np
 
 class NeuralNet:
 
-    def __init__(self, input_size, output_size, hidden_size, num_hidden, bias):
+    def __init__(self, input_size, output_size, hidden_size, num_hidden):
         # array of outputs through network
         self.y = []
         self.weightedsum = []
         self.delta = []
-        self.bias = np.zeros()
+        self.bias = []
         # build array of weight matrices
         self.weights = []
         for i in range(num_hidden):
             if i == 0:
                 self.weights.append(np.random.rand(input_size, hidden_size))
+                self.bias.append(np.zeros((input_size, hidden_size)))
             elif i == (num_hidden-1):
                 self.weights.append(np.random.rand(hidden_size, output_size))
+                self.bias.append(np.zeros((hidden_size, output_size)))
             else:
-                self.weights.append(np.random.rand(hidden_size,hidden_size))
-
-    # TODO: implement more advanced activation functions like leaky ReLU
+                self.weights.append(np.random.rand(hidden_size, hidden_size))
+                self.bias.append(np.zeros((hidden_size, hidden_size)))
 
     def relu(self, x):
         if x < 0:
@@ -46,31 +47,49 @@ class NeuralNet:
     def feed_forward(self, inputarray):
         for i in range(len(self.weights)):
             if i == 0:
-                self.weightedsum.append(np.dot(inputarray,self.weights[i]) + self.bias[i])
+                self.weightedsum.append(np.dot(inputarray, self.weights[i]) + self.bias[i])
                 self.y.append(self.relu(self.weightedsum[i]))
             else:
                 self.weightedsum.append(np.dot(self.y[i-1], self.weights[i]) + self.bias[i])
                 self.y.append(self.relu(self.weightedsum[i]))
 
-    def back_prop(self, trainingdata, expectedoutput):
+    def back_prop(self, input, expectedoutput):
         # get output layer error
-        error = expectedoutput - self.y[-1]
-        
-        self.delta.append((self.y[-1] - expectedoutput)*self.relu_d(self.weightedsum[-1])*self.y[-2])
-
-
-
-
-
+        output_layer = (self.y[-1] - expectedoutput)*self.relu_d(self.weightedsum[-1])
+        for x, e in reversed(list(enumerate(self.y))):
+            z_error = output_layer.dot(self.weights[x].T)
+            z_delta = z_error*self.relu_d(e)
+            if i == 0:
+                self.weights[x] += input.T.dot(z_delta)
+            else:
+                self.weights[x] += e.T.dot(output_layer)
 
     # TODO: write training function
-    def train(self, input, expectedoutput, output):
+    def train(self, input, expectedoutput):
+        self.feed_forward(input)
+        self.back_prop(input, expectedoutput, )
 
     # TODO: write function to save weights after training
+    def saveweights(self):
+        for i in range(len(self.weights)):
+            np.savetxt("w" + i + ".txt", self.weights[i], fmt="%s")
 
-    # TODO: write function to predict based on new input
+X = np.array([
+    0, 0,
+    0, 1,
+    1, 0,
+    1, 1
+]).reshape(4,2)
 
-    # TODO: write main controller
+Y = np.array([0, 1, 1, 0]).reshape(4, 1)
+NN = NeuralNet(2, 1, 3, 2)
+print(NN.weights)
+print(NN.bias)
+# for j in range(1000):
+#    for i in range(len(X)):
+        #NN.train(X, Y)
+
+
 
 
 
