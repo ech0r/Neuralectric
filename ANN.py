c031df5b0997e738f52bc39c23daca08f99f129e
ech0r <jrcoop34@gmail.com>
2018-11-14 09:10:52 -0800

    added tanh activation function

--- ANN.py
+++ ANN.py
@@ -11,7 +11,7 @@ class NeuralNet:
         self.layer_output_delta = []
         self.weightedsum = []
         self.weights = []
-        self.learningrate = 0.1
+        self.learningrate = 0.001
         for i in range(num_hidden + 1):
             if i == 0:
                 self.weights.append(np.random.rand(input_size, hidden_layer_size))
@@ -30,17 +30,27 @@ class NeuralNet:
         return 1 / (1 + np.exp(-x))
 
     def sigmoid_d(self, x):
-        return x*(1-x)
+        return x * (1.0 - x)
+
+    def tanh(self, x):
+        return np.tanh(x)
+
+    def tanh_d(self, x):
+        return 1 - x*x
 
     def act(self, x):
         if self.switch == "relu":
             return self.relu(x)
+        elif self.switch == "tanh":
+            return self.tanh(x)
         else:
             return self.sigmoid(x)
 
     def act_d(self, x):
         if self.switch == "relu":
             return self.relu_d(x)
+        elif self.switch == "tanh":
+            return self.tanh(x)
         else:
             return self.sigmoid_d(x)
 
@@ -66,16 +76,19 @@ class NeuralNet:
                 self.layer_output_delta.append(self.layer_output_delta[z-1]@self.weights[-z].T*self.act_d(self.weightedsum[-(z+1)]))
         self.layer_output_delta.reverse()
         for k in range(len(self.weights)):
-            self.weights[k] -= self.learningrate*self.layer_outputs[k].T@self.layer_output_delta[k]
+            buffer = self.weights[k]
+            delta = self.learningrate*self.layer_outputs[k].T@self.layer_output_delta[k]
+            self.weights[k] = np.subtract(self.weights[k], delta)
+            print(np.subtract(buffer, self.weights[k]))
 
-Net = NeuralNet(2, 1, 2, 1)
 
 
-for i in range(100):
+Net = NeuralNet(2, 1, 2, 1, "tanh")
+
+
+for i in range(1000):
     Net.feed_forward(np.atleast_2d(X))
     Net.back_prop(np.atleast_2d(X), np.atleast_2d(Y))
-    for h in Net.layer_outputs:
-        print(h.shape)
     Net.clear()
 
 Net.feed_forward(np.atleast_2d(X))
