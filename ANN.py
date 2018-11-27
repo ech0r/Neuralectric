be0c3f0f51b1486f012a58b415f4a02b31bb90cf
John Cooper <jrcoop34@gmail.com>
2018-11-27 00:54:03 -0800

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -21,13 +21,16 @@ class NeuralNet:
     def initialize_weights(self, x, y):
         input_size = x.shape[1]
         output_size = y.shape[1]
-        for i in range(self.num_hidden + 1):
-            if i == 0:
-                self.weights.append(np.random.uniform(low=0.0, high=1.0, size=(input_size, self.hidden_layer_size)))
-            elif i == self.num_hidden:
-                self.weights.append(np.random.uniform(low=0.0, high=1.0, size=(self.hidden_layer_size, output_size)))
+        for j in range(self.num_hidden + 1):
+            if j == 0:
+                #self.weights.append(np.random.uniform(low=0.0, high=0.5, size=(input_size, self.hidden_layer_size)))
+                self.weights.append(np.random.randn(input_size, self.hidden_layer_size))
+            elif j == self.num_hidden:
+                #self.weights.append(np.random.uniform(low=-, high=0.5, size=(self.hidden_layer_size, output_size)))
+                self.weights.append(np.random.randn(self.hidden_layer_size, output_size))
             else:
-                self.weights.append(np.random.uniform(low=0.0, high=1.0, size=(self.hidden_layer_size, self.hidden_layer_size)))
+                #self.weights.append(np.random.uniform(low=-0.5, high=0.5, size=(self.hidden_layer_size, self.hidden_layer_size)))
+                self.weights.append(np.random.randn(self.hidden_layer_size, self.hidden_layer_size))
 
     def relu(self, x):
         return np.where(x < 0, 0.01*x, x)
@@ -72,30 +75,31 @@ class NeuralNet:
         self.layer_outputs.append(self.act(np.atleast_2d(x)))
         self.weightedsum.append(np.atleast_2d(x))
         for z, e in enumerate(list(self.weights)):
-            self.weightedsum.append(np.atleast_2d(self.layer_outputs[z]@self.weights[z]))
+            self.weightedsum.append(np.atleast_2d(np.dot(self.layer_outputs[z], self.weights[z])))
             self.layer_outputs.append(np.atleast_2d(self.act(self.weightedsum[-1])))
 
-    def back_prop(self, x, y):
+    def back_prop(self, y):
         error = np.atleast_2d(y - self.layer_outputs[-1])
-        error_delta = np.atleast_2d(error*self.act_d(self.weightedsum[-1]))
-        for z, e in enumerate(reversed(list(self.weights))):
-            if z == 0:
+        error_delta = np.atleast_2d(error * self.act_d(self.layer_outputs[-1]))
+        for k in range(len(self.weights)):
+            if k == 0:
                 self.layer_output_delta.append(error_delta)
             else:
-                self.layer_output_delta.append(self.layer_output_delta[z-1]@self.weights[-z].T*self.act_d(self.weightedsum[-(z+1)]))
+                weighted_error = np.dot(self.layer_output_delta[-1], self.weights[-k].T)*self.act_d(self.weightedsum[-(k+1)])
+                self.layer_output_delta.append(weighted_error)
         self.layer_output_delta.reverse()
         for k in range(len(self.weights)):
-            buffer = self.weights[k]
-            delta = self.learningrate*self.layer_outputs[k].T@self.layer_output_delta[k]
-            self.weights[k] = np.subtract(self.weights[k], delta)
+            derivative = np.dot(self.layer_outputs[k].T, self.layer_output_delta[k])
+            self.weights[k] += derivative
 
 
-Net = NeuralNet(4, 8, "tanh")
+Net = NeuralNet(3, 1, "")
 Net.initialize_weights(X, Y)
+print(Net.weights)
 
-for i in range(1000):
+for i in range(10000):
     Net.feed_forward(np.atleast_2d(X))
-    Net.back_prop(np.atleast_2d(X), np.atleast_2d(Y))
+    Net.back_prop(np.atleast_2d(Y))
     Net.clear()
 
 Net.feed_forward(np.atleast_2d(X))
